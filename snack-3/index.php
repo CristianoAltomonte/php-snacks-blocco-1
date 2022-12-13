f8be6612037807ca0b4c9a80d7fd1412437970ad
CristianoAltomonte <cristianoaltomonte@hotmail.it>
2022-12-13 14:36:21 +0000

    Snack 3

---
+++ snack-3/index.php
@@ -0,0 +1,61 @@
+<!-- Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 
+e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+
+for ($i = 0; $i < count($posts); $i++) {
+    echo array_keys($posts)[$i] . '<br>';
+
+    $chiaveData = array_keys($posts)[$i];
+
+    for ($z = 0; $z < count($posts[$chiaveData]); $z++) {
+        echo '<b>' . $posts[$chiaveData][$z]['title'] .'</b>'  . '<br>';
+        echo $posts[$chiaveData][$z]['author'] . '<br>';
+        echo $posts[$chiaveData][$z]['text'] . '<hr>';
+    }
+}
+
+
+?>
